e82981eb2216ace22b7f3758a87d488382a1e5bc
Thomas Esteban Ariza Forero <thomas.esteban@gmail.com>
2024-09-03 18:52:22 -0500

    main2.py showing subprocess prints

--- app/main.py
+++ app/main.py
@@ -12,19 +12,29 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 logger = logging.getLogger(__name__)
 
+
 def print_current_time():
     colombia_tz = pytz.timezone('America/Bogota')
     current_time = datetime.now(colombia_tz).strftime("%Y-%m-%d %H:%M:%S")
     logger.info(f"Hora actual en Bogotá, Colombia: {current_time}")
 
+
 def scheduled_task():
     logger.info("Ejecutando la tarea programada...")
     try:
-        subprocess.run(["python", "/app/src/main2.py"])  # Ejecutar el subproceso
+        result = subprocess.run(
+            ["python", "/app/src/main2.py"],
+            capture_output=True,  # Capture stdout and stderr
+            text=True  # Ensure the output is returned as a string rather than bytes
+        )
+        # Log the stdout and stderr from main2.py
+        logger.info(f"Subproceso stdout: {result.stdout}")
+        logger.error(f"Subproceso stderr: {result.stderr}")
         logger.info("Subproceso ejecutado correctamente")
     except Exception as e:
         logger.error(f"Error al ejecutar el subproceso: {e}")
     logger.info("Tarea programada completada.")
+
 
 def run_scheduler():
     while True:
@@ -35,9 +45,11 @@ def run_scheduler():
             scheduled_task()
         time.sleep(25)  # Sleep for 25 seconds
 
+
 @app.route('/')
 def index():
     return 'La aplicación de webscrapping está funcionando'
+
 
 if __name__ == "__main__":
     threading.Thread(target=run_scheduler).start()
