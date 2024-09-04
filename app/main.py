51afd54102b977e47c2798e37b9b03dc21e775e3
Thomas Esteban Ariza Forero <thomas.esteban@gmail.com>
2024-09-03 19:06:47 -0500

    mein.py hora actualizada

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ def run_scheduler():
         print_current_time()
         colombia_tz = pytz.timezone('America/Bogota')
         current_time = datetime.now(colombia_tz)
-        if current_time.hour == 19 and current_time.minute == 5:
+        if current_time.hour == 19 and current_time.minute == 7:
             scheduled_task()
         time.sleep(25)  # Sleep for 25 seconds
 
